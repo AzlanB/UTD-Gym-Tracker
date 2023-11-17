cae022be04bbacc1a7971185635a466dee358836
Ross Wylie <crw200004@utdallas.edu>
2023-11-17 16:44:41 -0600

    Deleteing stuff

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,31 @@
 package main;
-import accounts.*;
-import gui.*;
-import recordmgr.*;
-
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class Main{
-    public static void main(String[] args){
-        Runtime rTime = Runtime.getRuntime();
-        String url = "D:/hi.html";
-        String browser = "C:/Program Files/Internet Explorer/iexplore.exe ";
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Specify the path to your HTML file
+            String htmlFilePath = "C:\\Users\\Rossw\\IdeaProjects\\SE-3354-Project\\src\\gui\\LogInPage.html";
+
+            // Open the HTML file in the default web browser
+            openInBrowser(htmlFilePath);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void openInBrowser(String htmlFilePath) throws IOException, URISyntaxException {
+        File htmlFile = new File(htmlFilePath);
+
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            Desktop.getDesktop().browse(htmlFile.toURI());
+        } else {
+            // Handle the case where the Desktop API is not supported
+            System.out.println("Desktop API is not supported on this platform.");
+        }
     }
 }
