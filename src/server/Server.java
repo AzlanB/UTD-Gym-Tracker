4ed83b2dc9a3dc797537ab9282e78d7c544d546a
Azlan Bokhari <axb210107@utdallas.edu>
2023-11-27 19:24:48 -0600

    Better connection handling, Server can now handle more than one Client

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,16 +1,51 @@
 package server;
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(4444);
-        Socket socket = server.accept();
-
-        // TODO: Code
-
-        socket.close();
+        server.setReuseAddress(true);
+        try {
+            while (true) {
+                Socket socket = server.accept();
+                new Thread(new ClientHandler(socket)).start();
+            }
+        } catch (Exception ignored) {}
         server.close();
     }
+
+    private record ClientHandler(Socket socket) implements Runnable {
+        @Override
+            public void run() {
+                PrintWriter toClient = null;
+                BufferedReader fromClient = null;
+                try {
+                    toClient = new PrintWriter(socket.getOutputStream(), true);
+                    fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String line;
+                    while ((line = fromClient.readLine()) != null) {
+                        // Temporary acknowledgement of input
+                        System.out.println("From " + socket.getInetAddress().getHostAddress() + ": " + line);
+                        // TODO: Register input and send appropriate output to client
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (toClient != null)
+                    toClient.close();
+                if (fromClient != null) {
+                    try {
+                        fromClient.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 }
