8ad8fe4d09a8fc7d3ae331798104db26bf40ca3f
Azlan Bokhari <axb210107@utdallas.edu>
2023-11-30 20:43:24 -0600

    Final Server-Side Implementation, Minor Changes to Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -38,47 +38,41 @@ public class Client {
         Scanner console = new Scanner(System.in);
         String line;
         while (!(line = console.nextLine()).equals("exit")) {
-            if (line.equals("register")) {
-                new RegistrationMenu(toServer, fromServer);
-            }
-            else if (line.equals("login")) {
-                System.out.print("Enter Username: ");
-                String username = console.nextLine();
-                System.out.print("Enter Password: ");
-
-                String hashedPassword = username + console.nextLine();
-                MessageDigest mD = MessageDigest.getInstance("SHA-256");
-                mD.update(hashedPassword.getBytes());
-                hashedPassword = new String(mD.digest());
-
-                toServer.println("login");
-                toServer.println(username);
-                toServer.println(hashedPassword);
-                System.out.println(fromServer.readLine());
-            }
-            else if (line.equals("submitRecord")) {
-                new SubmitRecordMenu(toServer, fromServer);
-            }
-            else if (line.equals("reviewRecord")) {
-                toServer.println("reviewRecord");
-                String response = fromServer.readLine();
-                System.out.println(response);
-                if (response.equals("Submitted Record:")) {
-                    for (int i = 0; i < 4; i++)
-                        System.out.println(fromServer.readLine());
-                    System.out.print("Accept or Deny: ");
-                    String review = console.nextLine().toLowerCase();
-                    if (review.equals("accept")) {
-                        System.out.print("Enter Score: ");
-                        String score = console.nextLine();
-                        toServer.println(review);
-                        toServer.println(score);
+            switch (line) {
+                case "register" -> new RegistrationMenu(toServer, fromServer);
+                case "login" -> {
+                    System.out.print("Enter Username: ");
+                    String username = console.nextLine();
+                    System.out.print("Enter Password: ");
+                    String hashedPassword = username + console.nextLine();
+                    MessageDigest mD = MessageDigest.getInstance("SHA-256");
+                    mD.update(hashedPassword.getBytes());
+                    hashedPassword = new String(mD.digest());
+                    toServer.println("login");
+                    toServer.println(username);
+                    toServer.println(hashedPassword);
+                    System.out.println(fromServer.readLine());
+                }
+                case "submitRecord" -> new SubmitRecordMenu(toServer, fromServer);
+                case "reviewRecord" -> {
+                    toServer.println("reviewRecord");
+                    String response = fromServer.readLine();
+                    System.out.println(response);
+                    if (response.equals("Submitted Record:")) {
+                        for (int i = 0; i < 4; i++)
+                            System.out.println(fromServer.readLine());
+                        System.out.print("Accept or Deny: ");
+                        String review = console.nextLine().toLowerCase();
+                        if (review.equals("accept")) {
+                            System.out.print("Enter Score: ");
+                            String score = console.nextLine();
+                            toServer.println(review);
+                            toServer.println(score);
+                        } else
+                            toServer.println(review);
                     }
-                    else
-                        toServer.println(review);
                 }
             }
-            // TODO: More Code
         }
 
         socket.close();
--- src/server/Server.java
+++ src/server/Server.java
@@ -36,62 +36,84 @@ public class Server {
 
                 String line;
                 while ((line = fromClient.readLine()) != null) {
-                    if (line.equals("register")) { // Format: "register", username, hashedPassword -> String success/error
-                        toClient.println(Database.register(fromClient.readLine(), fromClient.readLine()));
-                    }
-                    else if (line.equals("login")) { // Format: "login", username, hashedPassword -> String success/error
-                        String uN = fromClient.readLine();
-                        String response = Database.login(uN, fromClient.readLine());
-                        if (response.equals("Login Successful")) {
-                            loggedIn = true;
-                            username = uN;
+                    switch (line) {
+                        case "register" ->  // Format: "register", username, hashedPassword -> String success/error
+                            toClient.println(Database.register(fromClient.readLine(), fromClient.readLine()));
+                        case "login" -> {  // Format: "login", username, hashedPassword -> String success/error
+                            String uN = fromClient.readLine();
+                            String response = Database.login(uN, fromClient.readLine());
+                            if (response.equals("Login Successful")) {
+                                loggedIn = true;
+                                username = uN;
+                            }
+                            toClient.println(response);
                         }
-                        toClient.println(response);
-                    }
-                    else if (line.equals("getCategories")) { // Format: "getCategories" -> int amount, Category, Category, Category... etc.
-                        HashSet<String> categories = Database.getCategories();
-                        toClient.println(categories.size());
-                        for (String category : categories)
-                            toClient.println(category);
-                    }
-                    else if (line.equals("submitRecord")) { // Format: "submitRecord", category, description, proof -> String success/error
-                        String category = fromClient.readLine(), description = fromClient.readLine(), proof = fromClient.readLine();
-                        if (loggedIn)
-                            toClient.println(Database.submitRecord(username, category, description, proof));
-                        else
-                            toClient.println("Please login");
-                    }
-                    else if (line.equals("isAdmin")) { // Format: "isAdmin" -> boolean isAdmin / String "Please login"
-                        if (loggedIn)
-                            toClient.println(Database.isAdmin(username));
-                        else
-                            toClient.println("Please login");
-                    }
-                    else if (line.equals("reviewRecord")) { // Format "reviewRecord" -> String error / String success, username, category, description, proof -> "accept", int score / "deny" -> void
-                        if (loggedIn) {
-                            if (Database.isAdmin(username)) {
-                                String[] record = Database.reviewRecord();
-                                if (record != null) {
-                                    toClient.println("Submitted Record:");
-                                    for (int i = 0; i < 4; i++)
-                                        toClient.println(record[i]);
-
-                                    String review = fromClient.readLine();
-                                    int score = 0;
-                                    if (review.equals("accept"))
-                                        score = Integer.parseInt(fromClient.readLine());
-                                    Database.reviewRecord(review, score);
+                        case "getRecordHistory" -> { // Format: "getRecordHistory" -> String success/error, int amount, String category1, description1, proof1, category2... etc.
+                            if (loggedIn) {
+                                ArrayList<String[]> records = Database.getRecordHistory(username);
+                                int size = records.size();
+                                toClient.println("Record History:");
+                                toClient.println(size);
+                                for (int i = size - 1; i >= 0; i--) {
+                                    String[] record = records.get(i);
+                                    toClient.println(record[0]);
+                                    toClient.println(record[1]);
+                                    toClient.println(record[2]);
                                 }
-                                else
-                                    toClient.println("No submitted records or record review in progress");
                             }
                             else
-                                toClient.println("User lacks admin permissions");
+                                toClient.println("Please log in");
                         }
-                        else
-                            toClient.println("Please login");
+                        case "getLeaderboard" -> { // Format: "getLeaderboard", category -> int amount, String username1, description1, proof1, username2... etc.
+                            ArrayList<String[]> leaderboard = Database.getLeaderboard(fromClient.readLine());
+                            toClient.println(leaderboard.size());
+                            for (String[] record : leaderboard) {
+                                toClient.println(record[0]);
+                                toClient.println(record[1]);
+                                toClient.println(record[2]);
+                            }
+                        }
+                        case "getCategories" -> {  // Format: "getCategories" -> int amount, String category1, category2, category3... etc.
+                            HashSet<String> categories = Database.getCategories();
+                            toClient.println(categories.size());
+                            for (String category : categories)
+                                toClient.println(category);
+                        }
+                        case "submitRecord" -> {  // Format: "submitRecord", category, description, proof -> String success/error
+                            String category = fromClient.readLine(), description = fromClient.readLine(), proof = fromClient.readLine();
+                            if (loggedIn)
+                                toClient.println(Database.submitRecord(username, category, description, proof));
+                            else
+                                toClient.println("Please log in");
+                        }
+                        case "isAdmin" -> {  // Format: "isAdmin" -> boolean isAdmin / String "Please log in"
+                            if (loggedIn)
+                                toClient.println(Database.isAdmin(username));
+                            else
+                                toClient.println("Please log in");
+                        }
+                        case "reviewRecord" -> {  // Format "reviewRecord" -> String error / String success, username, category, description, proof -> "accept", int score / "deny" -> void
+                            if (loggedIn) {
+                                if (Database.isAdmin(username)) {
+                                    String[] record = Database.reviewRecord();
+                                    if (record != null) {
+                                        toClient.println("Submitted Record:");
+                                        for (int i = 0; i < 4; i++)
+                                            toClient.println(record[i]);
+
+                                        String review = fromClient.readLine();
+                                        int score = 0;
+                                        if (review.equals("accept"))
+                                            score = Integer.parseInt(fromClient.readLine());
+                                        Database.reviewRecord(review, score);
+                                    } else
+                                        toClient.println("No submitted records or record review in progress");
+                                } else
+                                    toClient.println("User lacks admin permissions");
+                            } else
+                                toClient.println("Please log in");
+                        }
                     }
-                    // TODO: Register more inputs and send appropriate outputs to client
                 }
             } catch (IOException ignored) {} catch (InterruptedException e) { e.printStackTrace(); }
             if (toClient != null)
